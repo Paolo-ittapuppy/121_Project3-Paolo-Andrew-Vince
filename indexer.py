a8b2977c55b2c0ba944894aeb77c9ed96b207a37
paolo <uranipa@gmail.com>
2023-05-10 13:43:49 -0700

    skeleton code

---
+++ indexer.py
@@ -0,0 +1,40 @@
+import sqlite3
+import json
+from bs4 import BeautifulSoup
+import pathlib
+import os
+from collections import defaultdict
+#idea: go through some number of files, start indexing words to websties, transition that into a sqlite format, then restart
+#dictionary, and keep going. 
+
+def reverseIndex(dir:str, RIndDB:str, docMapDB:str):
+    counter = 0
+    maxCounter = 300
+    index = defaultdict(list)
+    maping = {}
+    currentID = 1
+    #format of reverse index: {word:[()]}
+    for path in os.listdir(dir):
+        for file in os.listdir(path):
+            counter +=1 
+            readHTML(file, index)
+            if counter >= maxCounter:
+                storeIndexes(RIndDB, index)
+                storeDocMap(docMapDB, maping)
+            currentID +=1
+            pass
+
+        storeIndexes()
+        #reset data
+
+
+def storeIndexes(file, data):
+    pass
+
+def storeDocMap(file, data):
+    pass
+
+def readHTML(path, index):
+    #update the index, adding the path to the list of the words key
+    pass
+    
